1ea134788aaa65df9f601682d88973c582d11c4f
Arnas Rutkauskas <arnas@totemmaker.net>
2022-02-03 17:45:23 +0200

    Add channel & color definitions

---
+++ libraries/TotemModules/include/Channels.h
@@ -0,0 +1,17 @@
+#ifndef CHANNELS
+#define CHANNELS
+
+enum Channel {
+    chA = 0,
+    chB,
+    chC,
+    chD,
+    chE,
+    chF,
+    chG,
+    chH,
+
+    chAll = 0xFF
+};
+
+#endif /* CHANNELS */
