eb8e56a270082b0aae614c5ed98a86497e98259f
Arnas Rutkauskas <arnas@totemmaker.net>
2023-10-12 17:50:55 +0300

    Remove legacy code

--- libraries/TotemBUS/include/TotemModuleList.h
+++
@@ -1,9 +0,0 @@
-#ifndef TOTEMMODULELIST
-#define TOTEMMODULELIST
-
-#include "module/Module11.h"
-#include "module/Module14.h"
-#include "module/Module15.h"
-#include "module/Module22.h"
-
-#endif /* TOTEMMODULELIST */
